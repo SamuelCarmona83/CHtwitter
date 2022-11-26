5565cbc50d64ede9d53f2cdc5a07ae29beebacb0
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2022-11-26 00:02:49 +0000

    :new: Nuestra nueva plantilla!

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+import arrow
+import datetime
 
 db = SQLAlchemy()
 
@@ -7,13 +9,71 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    profile_name = db.Column(db.String(50), unique=False, nullable=False)
+    #tweets gracias al backref
+
+    def __init__(self, email, password, username, profile_name):
+        self.email = email
+        self.password = password
+        self.username = username
+        self.profile_name = profile_name
+
+        #opcional
+        self.is_active = True
+
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            #"id": self.id,
+            #"email": self.email,
+            "username": self.username,
+            "profile": self.profile_name,
             # do not serialize the password, its a security breach
+        }
+
+    def get_profile(self):
+        return {
+            #"id": self.id,
+            #"email": self.email,
+            "username": '@'+self.username,
+            "profile": self.profile_name,
+            # do not serialize the password, its a security breach
+            "tuits": [ tuit.get_content() for tuit in self.tweets ]
+        }
+
+class Tweet(db.Model): #Query
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    content = db.Column(db.String(280), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+
+    #foreing key siempre del lado N
+    author_id = db.Column( db.Integer, db.ForeignKey('user.id'))
+
+    author = db.relationship("User", backref="tweets")
+
+    def __init__(self, content, author):
+        self.content = content
+        self.date = datetime.datetime.today()
+        self.author = author
+
+    def __repr__(self):
+        return '<Tweet => %r>' % self.id
+    
+    def serialize(self):
+        return {
+            "content": self.content,
+            "date": arrow.get(self.date).humanize(),
+            "author": self.author.serialize() if self.author != None else 'No author'
+        }
+
+    def get_content(self):
+        return {
+            "content": self.content,
+            "date": arrow.get(self.date).humanize(),
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,83 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Tweet
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user', methods=['GET','POST'])
+def handle_user():
+    if request.method == 'GET':
+        response_body = {
+            "msg": "Hello, this is your GET /user response 😎"
+        }
+        return jsonify(response_body), 200
+    else:
+        body = request.json
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        if 'email' not in body:
+            return 'El usuario no tiene email', 400
+        if 'password' not in body:
+            return 'El usuario no tiene password', 400
+        if 'username' not in body:
+            return 'El usuario no tiene username', 400
+        if 'profile_name' not in body:
+            return 'El usuario no tiene profile_name', 400
+        
+        profile = User.query.filter_by(email=body['email']).one_or_none()
 
-    return jsonify(response_body), 200
+        if profile == None:
+
+            new_user = User(body['email'], body['password'], body['username'], body['profile_name'])
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                return ""
+            except Exception as err:
+                return 'Ha ocurrido un error!💥', 500
+            #User(email, password, username, profile_name)
+        else:
+            return "Ya existe un usuario con ese email!", 400
+
+        return "Method not implemented yet!",500
+
+@api.route('/profile/<string:user_name>', methods=['GET'])
+def get_profile(user_name):
+    profile = User.query.filter_by(username=user_name).one_or_none()
+    if profile == None:
+        return 'Ese usuario no existe ❌', 404
+    else:
+        return jsonify(profile.get_profile()), 200
+
+
+@api.route('/tweets', methods=['GET'])
+def get_tweets():
+    all_tweets = Tweet.query.all()
+    return jsonify(
+            [ tweet.serialize() for tweet in all_tweets ]
+        ), 200
+
+
+@api.route('/tweets', methods=['POST'])
+def post_tweet():
+    body = request.json
+    if "content" not in body:
+        return "Ese tuit no tiene contenido! ⛔", 400
+    if "email" not in body:
+        return "Aqui no se permite el anonimato, incluye un email. ✝", 400
+    else:
+        author = User.query.filter_by(email=body["email"]).one_or_none()
+        if author == None:
+            return "Ese usuario no existe en tuiter.", 404
+        else:
+            new_tweet = Tweet(body["content"], author)
+            db.session.add(new_tweet) #Memoria RAM
+            try:
+                db.session.commit() #Guarda en datos solidos!
+                return "Tuit creado con exito! 🦄", 201
+            except Exception as err:
+                return "Ocurrio un error en el servidor 🐬", 500
+        #return jsonify(new_tweet.serialize()), 201
+    return "Error algo ah ocurrido! 🐋", 404
