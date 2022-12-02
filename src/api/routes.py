7bf724680b879471e9f6270eedb1dfdcebba0e50
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2022-12-02 22:24:23 +0000

    :zap: We can loging & hash passwords

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,13 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Tweet
 from api.utils import generate_sitemap, APIException
+import hashlib
 
+
+#Importaciones de JWT
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -29,10 +34,9 @@ def handle_user():
             return 'El usuario no tiene profile_name', 400
         
         profile = User.query.filter_by(email=body['email']).one_or_none()
-
         if profile == None:
-
-            new_user = User(body['email'], body['password'], body['username'], body['profile_name'])
+            new_user = User(body['email'], hashlib.md5( body['password'].encode() ).hexdigest(), body['username'], body['profile_name'])
+            
             try:
                 db.session.add(new_user)
                 db.session.commit()
@@ -58,7 +62,7 @@ def get_profile(user_name):
 def get_tweets():
     all_tweets = Tweet.query.all()
     return jsonify(
-            [ tweet.serialize() for tweet in all_tweets ]
+            list(reversed([ tweet.serialize() for tweet in all_tweets ]))
         ), 200
 
 
@@ -68,10 +72,8 @@ def post_tweet():
     body = request.json
     if "content" not in body:
         return "Ese tuit no tiene contenido! ⛔", 400
-    if "email" not in body:
-        return "Aqui no se permite el anonimato, incluye un email. ✝", 400
     else:
-        author = User.query.filter_by(email=body["email"]).one_or_none()
+        author = User.query.filter_by(username=get_jwt_identity()).one_or_none()
         if author == None:
             return "Ese usuario no existe en tuiter.", 404
         else:
@@ -84,3 +86,24 @@ def post_tweet():
                 return "Ocurrio un error en el servidor 🐬", 500
         #return jsonify(new_tweet.serialize()), 201
     return "Error algo ah ocurrido! 🐋", 404
+
+@api.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username == None or password == None:
+        return jsonify({"msg": "Bad username or password"}), 401
+    else:
+        profile = User.query.filter_by(username=username, password=hashlib.md5( password.encode() ).hexdigest()).one_or_none()
+        if profile == None:
+            return 'El usuario no esta registrado en Chuiter', 404
+        else:
+            access_token = create_access_token(identity=username)
+            return jsonify({"token": access_token })
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
--- src/app.py
+++ src/app.py
@@ -16,7 +16,9 @@ from api.commands import setup_commands
 #jwt imports
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
+
 from flask_jwt_extended import jwt_required
+
 from flask_jwt_extended import JWTManager
 
 #from models import Person
@@ -74,26 +76,6 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0 # avoid cache memory
     return response
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json.get("username", None)
-
-    password = request.json.get("password", None)
-
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    access_token = create_access_token(identity=username)
-    return jsonify({"token": access_token })
-
-@app.route("/protected", methods=["GET"])
-@jwt_required()
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
